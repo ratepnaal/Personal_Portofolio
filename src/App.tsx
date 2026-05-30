df9b8a59ad1e9df84923cdc1b48335371dd33e35
ratepnaal <ratepn445@gmail.com>
2026-05-31 00:43:31 +0300

    fix bug deployment +1

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { Header } from './components/Header/Header'
 import { Main } from './components/Main/Main'
 import { GlobalStyle } from './styles/global'
 import { Analytics } from "@vercel/analytics/react"
-import 'react-toastify/dist/ReactToastify.css'
 function App() {
   return (
     <>
