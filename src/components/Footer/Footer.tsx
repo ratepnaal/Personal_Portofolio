02257a0575c265f7f35478d09f85c78469b42818
ratepnaal <ratepn445@gmail.com>
2026-05-31 01:09:11 +0300

    code cleaner

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,8 @@
 import { Container } from './styles'
-import reactIcon from '../../assets/react-icon.svg'
 import linkedin from '../../assets/linkedin.svg'
 import githubIcon from '../../assets/github.svg'
 import whatsapp from '../../assets/whatsapp.svg'
 import telegram from '../../assets/telegram.svg'
-import instagramIcon from '../../assets/instagram.svg'
-
 export function Footer() {
   return (
     <Container className="footer">
